d14295be094b55441c051c8dce835e64ef4140dd
Yulia Grineva <jgrinja@gmail.com>
2022-04-22 18:42:12 +0200

    part 2-4

--- database/db.js
+++ database/db.js
@@ -1,14 +1,83 @@
 const spicedPg = require("spiced-pg");
 const db = spicedPg("postgres:postgres:postgres@localhost:5432/petition");
 
+const bcrypt = require("bcryptjs");
+
 module.exports.getAllSignatures = () => db.query("SELECT * FROM signatures");
 
-module.exports.addSigns = (firstname, lastname, signature) => {
-    const query = `
-        INSERT INTO signatures (firstname, lastname, signature)
-        VALUES ($1, $2, $3)
-        RETURNING *
-    `;
-    const params = [firstname, lastname, signature];
+module.exports.getAllSignersProfiles = () =>
+    db.query(
+        "SELECT users.firstname AS firstname, users.lastname AS lastname, city, age, url FROM users RIGHT JOIN signatures ON users.id = signatures.user_id FULL OUTER JOIN profiles ON users.id = profiles.user_id"
+    );
+module.exports.getCitySigners = ({ city }) => {
+    const query = `SELECT * FROM users
+JOIN signatures
+ON users.id = signatures.user_id
+JOIN profiles
+ON users.id = profiles.user_id
+WHERE LOWER(profiles.city) = LOWER($1)`;
+
+    const params = [city];
     return db.query(query, params);
 };
+
+module.exports.addSigns = (firstname, lastname, signature, user_id) => {
+    const query = `
+        INSERT INTO signatures (firstname, lastname, signature, user_id)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+    const params = [firstname, lastname, signature, user_id];
+    return db.query(query, params);
+    // req.session.signatureId = id;
+};
+
+function hashPassword(password) {
+    return bcrypt.genSalt().then((salt) => {
+        return bcrypt.hash(password, salt);
+    });
+}
+
+// module.exports.getUserById = (id) => {
+
+// };
+
+module.exports.addUser = ({ firstname, lastname, email, password }) => {
+    return hashPassword(password).then((password_hash) => {
+        const query = `
+       INSERT INTO users (firstname, lastname, email, password_hash)
+       VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+        const params = [firstname, lastname, email, password_hash];
+        return db.query(query, params);
+    });
+};
+
+function getUserByEmail(email) {
+    const query = `SELECT * FROM users WHERE email = $1`;
+    const params = [email];
+    const foundUser = db.query(query, params);
+    return foundUser;
+}
+
+module.exports.checkLogin = ({ email, password }) => {
+    return getUserByEmail(email).then((result) => {
+        const foundUser = result.rows[0];
+        console.log("HERE", foundUser);
+        if (!foundUser) {
+            return null;
+        }
+        return bcrypt
+            .compare(password, foundUser.password_hash)
+            .then(function (match) {
+                if (match) {
+                    return foundUser;
+                } else {
+                    return null;
+                }
+            });
+    });
+};
+
+// WHERE LOWER(profiles.city) = LOWER($1);
--- database/signatures.sql
+++ database/signatures.sql
@@ -1,17 +1,69 @@
+
+DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS signatures;
 
-CREATE TABLE signatures (
+DROP TABLE IF EXISTS users;
+
+
+
+CREATE TABLE users (
     id SERIAL primary key,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (firstname, lastname, email, password_hash) 
+VALUES ('Yulia', 'Grineva', 'yu@gmail.com', 'hifhibwhebihbfc');
+
+INSERT INTO users (firstname, lastname, email, password_hash) 
+VALUES ('Angela', 'Merkel', 'merk@gmail.com', 'hifhiddbwhebihbfc');
+
+INSERT INTO users (firstname, lastname, email, password_hash) 
+VALUES ('Olaf', 'Sholz', 'sholz@gmail.com', 'hifhikjsbwhebihbfc');
+
+
+
+
+CREATE TABLE signatures (
+    id SERIAL primary key,
+    user_id INT NOT NULL UNIQUE REFERENCES users(id),
     signature TEXT
 );
 
-INSERT INTO signatures (firstname, lastname, signature) 
-VALUES ('Yulia', 'Grineva', 'sign');
+INSERT INTO signatures (signature, user_id) 
+VALUES ('sign', 1);
 
-INSERT INTO signatures (firstname, lastname, signature) 
-VALUES ('Angela', 'Merkel', 'sign2');
+INSERT INTO signatures ( signature, user_id) 
+VALUES ('sign2', 2);
 
-INSERT INTO signatures (firstname, lastname, signature) 
-VALUES ('Olaf', 'Scholz', 'sign3');
+INSERT INTO signatures (signature, user_id) 
+VALUES ('sign3', 3);
+
+CREATE TABLE profiles (
+    id SERIAL primary key,
+    age INT,
+    city VARCHAR,
+    url VARCHAR,
+    user_id INT NOT NULL UNIQUE REFERENCES users(id)
+);
+
+INSERT INTO profiles (age, city, url, user_id) 
+VALUES (68, 'Berlin', 'https://spiced.space/aspartame/petition/', 1);
+
+INSERT INTO profiles (age, city, url, user_id) 
+VALUES (78, 'Berlin', 'https://translate.google.com/', 2);
+
+INSERT INTO profiles (age, city, url, user_id) 
+VALUES (88, 'Berlin', 'https://www.amazon.de/', 3);
+
+
+SELECT * FROM users
+JOIN signatures
+ON users.id = signatures.user_id
+JOIN profiles
+ON users.id = profiles.user_id
+WHERE profiles.city = 'Berlin';
+
